71c639012d9f749ac316727dd2e098f176732f17
SantiagoxEscobar <santiago.escobar97@gmail.com>
2025-01-04 23:53:41 -0300

    Clases Cuenta y Prestamo

--- src/entidad/Prestamo.java
+++ src/entidad/Prestamo.java
@@ -18,4 +18,109 @@ public class Prestamo {
     private boolean deleted;
     private LocalDate deleteDate;
     
+    
+    // Constructor 
+	public Prestamo(long id, String observaciones, Cliente cliente, Cuenta cuenta, LocalDate fechaAlta,
+			BigDecimal importe, int cuotas, BigDecimal valorCuotas, String estado, LocalDate createDate,
+			boolean deleted, LocalDate deleteDate) {
+		super();
+		this.id = id;
+		this.observaciones = observaciones;
+		this.cliente = cliente;
+		this.cuenta = cuenta;
+		this.fechaAlta = fechaAlta;
+		this.importe = importe;
+		this.cuotas = cuotas;
+		this.valorCuotas = valorCuotas;
+		this.estado = estado;
+		this.createDate = createDate;
+		this.deleted = deleted;
+		this.deleteDate = deleteDate;
+	}
+	
+	
+	// Getters y Setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Cuenta getCuenta() {
+		return cuenta;
+	}
+	public void setCuenta(Cuenta cuenta) {
+		this.cuenta = cuenta;
+	}
+	public LocalDate getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(LocalDate fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	public BigDecimal getImporte() {
+		return importe;
+	}
+	public void setImporte(BigDecimal importe) {
+		this.importe = importe;
+	}
+	public int getCuotas() {
+		return cuotas;
+	}
+	public void setCuotas(int cuotas) {
+		this.cuotas = cuotas;
+	}
+	public BigDecimal getValorCuotas() {
+		return valorCuotas;
+	}
+	public void setValorCuotas(BigDecimal valorCuotas) {
+		this.valorCuotas = valorCuotas;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+	public boolean isDeleted() {
+		return deleted;
+	}
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	public LocalDate getDeleteDate() {
+		return deleteDate;
+	}
+	public void setDeleteDate(LocalDate deleteDate) {
+		this.deleteDate = deleteDate;
+	}
+	
+	
+	// ToString
+	@Override
+	public String toString() {
+		return "Prestamo [id=" + id + ", observaciones=" + observaciones + ", cliente=" + cliente + ", cuenta=" + cuenta
+				+ ", fechaAlta=" + fechaAlta + ", importe=" + importe + ", cuotas=" + cuotas + ", valorCuotas="
+				+ valorCuotas + ", estado=" + estado + ", createDate=" + createDate + ", deleted=" + deleted
+				+ ", deleteDate=" + deleteDate + "]";
+	}
+    
 }
