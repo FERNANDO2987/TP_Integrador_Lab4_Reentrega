0f076a97c92429ae9b78ef52c223dcdf5a8706f5
maximo0001 <maximo148601@gmail.com>
2025-02-11 19:06:00 -0300

    Update Principal.java

--- TPInt_Grupo1_Lab4/src/principal/Principal.java
+++ TPInt_Grupo1_Lab4/src/principal/Principal.java
@@ -1,7 +1,10 @@
 package principal;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import datosImpl.PrestamoDaoImpl;
+import entidad.Cliente;
+import entidad.Cuenta;
 import entidad.Prestamo;
 import entidad.Provincia;
 import negocio.PrestamoNeg;
@@ -14,8 +17,22 @@ public class Principal {
     public static void main(String[] args) {
 
     	
+    	
     
         PrestamoNeg impl = new PrestamoNegImpl();
+        
+        Prestamo pr = new Prestamo();
+        Cliente cl = new Cliente();
+        Cuenta cu = new Cuenta();
+        cu.setNroCuenta(222222);
+        cl.setId(2);
+        
+        pr.setCliente(cl);
+        pr.setCuenta(cu);
+        pr.setImporte(BigDecimal.valueOf(7000));
+        pr.setCuotas(3);
+        impl.AgregarPrestamo(pr);      
+        
         ArrayList<Prestamo> prestamos = (ArrayList<Prestamo>) impl.ListarPrestamos();
         		
         
@@ -32,6 +49,7 @@ public class Principal {
             System.out.println(p.getCuotas());
             System.out.println(p.getObservaciones());
             System.out.println(p.getEstado());
+            System.out.println("----------------------------------------------");
         }
         
         
@@ -39,3 +57,4 @@ public class Principal {
     
     }
 }
+ 
