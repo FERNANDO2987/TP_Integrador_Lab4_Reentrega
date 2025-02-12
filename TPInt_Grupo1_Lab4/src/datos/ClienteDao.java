f49802a3448a0b3021c4b028b884bd0aa7409373
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2025-02-11 21:24:23 -0300

    comentar ModificarCliente ClienteDaoImpl
    lo tuve que comentar porque habia dos metodos llamados igual y entraban en conflicto

--- TPInt_Grupo1_Lab4/src/datos/ClienteDao.java
+++ TPInt_Grupo1_Lab4/src/datos/ClienteDao.java
@@ -6,14 +6,11 @@ import entidad.Cliente;
 import entidad.UsuarioCliente;
 
 public interface ClienteDao {
-	public boolean modificarCliente(Cliente cliente);
 	public List<Cliente> ObtenerClientes();
 	public boolean agregarOmodifcarCliente(Cliente cliente);
 	public boolean modificarCliente(Cliente cliente);
 	public boolean eliminarCliente(int idCliente);
 	public boolean agregarUsuarioCliente(UsuarioCliente usuarioCliente);
-	
-	
 	public boolean existeDni(String dni);
 	public boolean existeCuil(String cuil);
 	public boolean existeCorreo(String correo);
